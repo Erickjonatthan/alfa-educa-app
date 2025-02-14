5da4d205c9113bfce093786e74afe8599d03763c
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-02-13 21:53:57 -0300

    adiciona controladores para gerenciamento de usuários, incluindo edição, exclusão, listagem e mudança de função, além de estilos para a interface do usuário

---
+++ controllers/usuario/enviarImagem.ts
@@ -0,0 +1,54 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
+import User from "@/context/User";
+
+export const enviarImagem = async (base64Image: string | null, user: User | null, setUser: (user: User) => void, setImage: (image: string | null) => void, setTempImage: (image: string | null) => void, setBase64Image: (image: string | null) => void) => {
+  if (base64Image) {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      const userId = user?.id;
+
+      const response = await fetch(
+        `https://alfa-educa-server.onrender.com/cadastro`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            id: userId,
+            imgPerfil: base64Image,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const updatedUser: User = {
+          ...user,
+          id: userId as string, // Garantir que id seja uma string
+          nome: user?.nome || "", // Garantir que nome seja uma string
+          email: user?.email || "", // Garantir que email seja uma string
+          imgPerfil: `data:image/jpeg;base64,${base64Image}`,
+          nivel: user?.nivel || 0, // Garantir que nivel seja um número
+          isAdmin: user?.isAdmin || false, // Garantir que isAdmin seja um boolean
+          pontos: user?.pontos || 0, // Garantir que pontos seja um número
+        };
+        setUser(updatedUser); // Atualiza o contexto do usuário
+        setImage(`data:image/jpeg;base64,${base64Image}`); // Atualiza a imagem de perfil permanentemente
+        setTempImage(null); // Limpa a imagem temporária
+        setBase64Image(null); // Limpa a imagem base64
+        Alert.alert("Sucesso", "Imagem enviada com sucesso!");
+      } else {
+        const errorData = await response.text();
+        console.error("Erro ao enviar imagem:", errorData);
+        Alert.alert("Erro", "Falha ao enviar imagem.");
+      }
+    } catch (error) {
+      console.error("Erro ao enviar imagem:", error);
+      Alert.alert("Erro", "Erro ao conectar com o servidor.");
+    }
+  } else {
+    Alert.alert("Erro", "Nenhuma imagem selecionada.");
+  }
+};
